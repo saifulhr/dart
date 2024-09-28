bef56f9a6562d8c08cc2bdabb2a5863715de541a
saifulhr <saifulhr006@gmail.com>
2024-09-28 17:00:46 +0600

    dart code

---
+++ hridoy.dart
@@ -0,0 +1,202 @@
+// void main(){
+//   bazarlist(100,200,300,15,1000);
+// }
+// void bazarlist(int rice , int oil , int fish, int coke ,int assets){
+//   var expense =rice+oil+fish+coke;
+//   var totalAssets= assets-expense;
+//   print(expense);
+//   print(totalAssets);
+// }
+
+// void main(){
+//   for(int i=0; i < 50; i++){
+//     print(int);
+//   }
+
+// }
+// void main() {
+//   List<String> fruits = ['Apple', 'Banana', 'Cherry'];
+
+//   for (int i = 0; i < fruits.length; i++) {
+//     print(fruits[i]);
+//   }
+// }
+// import 'dart:math';
+
+// void main(){
+//    kotodece(1000,2000,1500,5000,1200,300,1000,3000);
+  
+// } 
+
+// void kotodece(int A, int B, int C, int D, int e, int F, int G,int mamunredichi){
+//   var total=A + B+ C+ D+ e+ F+ G;
+//   var akhonache=total-mamunredichi;
+//   print(total);
+//   print(akhonache);
+// } 
+
+
+// this is list 
+
+
+// void main(){
+//   var citynames=['Dhaka','Khulna', 'Rajshai', 'Borishal','Gopalgange'];
+
+//   // print(citynames);
+//   print(citynames[0]);
+// }
+
+
+
+
+// this is a map section ?
+
+// void main(){
+//   var person={
+//     'name' : 'Jobbar Sheikh',
+//      'Age' : '43',
+//      'City': 'Barishal',
+//      'Height':'5 feet 7 inch',
+//   };
+//   // print(person);
+//   print(person['name']);
+
+// }
+
+   
+// List Properties......?
+
+// void main(){
+//   var citynames=['Dhaka','Khulna', 'Rajshai', 'Borishal','Gopalgange'];
+
+  // var result=citynames.isNotEmpty;
+  // print(citynames);
+  // citynames.add("Coxbazar");
+//   print(citynames);
+// }
+
+
+// this is a set section ?
+
+// void main(){
+
+// var A=['Khulna', 4];
+//   // A.add('Dhaka');
+//   A[1]= 40;
+//   print(A);
+// }
+
+// this is if else section
+
+// import 'dart:developer';
+
+// void main(){
+//   var exammarks=100;
+//   if(exammarks<=90){
+// print("you got A+ ");
+//   }
+//   else if(exammarks<=75){
+// print("You got A");
+//   }
+//   else if(exammarks<=60){
+//     print("You got A-");
+//   }
+//   else if(exammarks<=50){
+//     print("you got B");
+//   }
+//   else if(exammarks<=33){
+//     print("You got C");
+//   }
+//   else{
+//     print("Your result is fall");
+//   }
+// }
+
+// void main(){
+//   var chicken=400;
+//   switch(chicken){
+//     case 100 :
+//     print('Buy 4 chicken');
+//     break;
+//     case 200:
+//     print('Buy 2 chicken');
+//     break;
+//     case 300:
+//     print('Buy 1.5 kg chicken');
+//     break;
+//     // case 400:
+//     // print('Buy 1 chicken');
+//     default:
+//     print('Condition not matched in Chicken Price');
+//     break;
+//   }
+// }
+
+// void main(){
+//   var Birdprice=2000;
+//   if (Birdprice<1000) {
+//     print('Buy 1 pair bird');
+//   }
+//   else if(Birdprice<=1000){
+// print('Buy 2 pair birds ');
+//   }
+//   else if(Birdprice<=500){
+//     print('Buy single male pis birds');
+//   }
+//   else{
+//     print('Price not matched dont buy any pairs');
+//   }
+
+// }
+
+// void main(){
+// birdslist(4000,4500,175,200,600,500,150,40,13000);
+// }
+// void birdslist(
+//   int birds, int cage, eggfood, int waterpot, int minarelblock, int breedingblock,int softfood , int medichine,int cash
+// ){
+//   var allexpense= birds+cage+eggfood+waterpot+minarelblock+breedingblock+softfood+medichine;
+//   var total=cash-allexpense;
+//   print(allexpense);
+//   print(total);
+// }
+
+// void main(){
+//  for(var i=0; i<100;i=i+1)
+//  print(i);
+// }
+
+
+// A(){
+//   var x=10;
+//   var y=20;
+//   print(x+y);}
+//   void main(){
+//     A();
+//   }
+
+  // int A(int a,int b){
+  //   return a+b;
+  // }
+  // void main(){
+  //   int result =A(20,20);
+  //   print(result);
+  // }
+
+
+// A(){
+//   var a=10;
+//   var b=20;
+//   print(a+b);
+// }
+// void main(){
+//   A();
+// }
+
+// int add(int a,int b){
+//   return a*b;
+// }
+// void main(){
+//   int result=add(10,10);
+//   print(result);
+// }
